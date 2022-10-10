fd326787653da306b69f656530352fe101139983
ZakiaHussaini <zakiahussaini19@gmail.com>
2022-10-10 11:08:26 +0000

    create is_invalid_letter to validate user input

--- run.py
+++ run.py
@@ -39,10 +39,16 @@ class Hangman():
         self.guess = guess
         self.game_completion = list('_' * len(self.guess))
 
-     def find_indexes(self, letter):
+    def find_indexes(self, letter):
         """
         this method take a letter and return a list with its index in the 
         word to guess.
         """
         return [i for i, char in enumerate(self.guess) if letter == char]
 
+    def is_invalid_letter(self, input_):
+        """
+        this method validate the user input to check the input is just a letter
+        not number or text with more than 1 char
+        """
+        return input_.isdigit() or (input_.isalpha() and len(input_) > 1)
